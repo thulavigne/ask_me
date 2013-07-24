9e18db30c48700a3087f99f814bbeba61f688664
Thu Quach <thuquach90@gmail.com>
2013-07-24 09:19:15 -0700

    Add title functionality for show page.

--- spec/integration/creating_discussions_spec.rb
+++ spec/integration/creating_discussions_spec.rb
@@ -7,5 +7,9 @@ feature 'Creating Discussions' do
     fill_in 'Question', :with => 'TextMate 2'
     click_button 'Create Discussion'
     page.should have_content('Discussion has been created.')
+    discussion = Discussion.find_by_question("TextMate 2")
+    page.current_url.should == discussion_url(discussion)
+    title = "TextMate 2 - Discussions - Ask Me"
+    find("title").should have_content(title)
   end
 end
