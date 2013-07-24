48d956591d4c8e0f09214cbcbd71d4852ccd758a
Thu Quach <thuquach90@gmail.com>
2013-07-24 08:54:25 -0700

    Generate Rails 3 Collaborative Q&A Site App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AskMe::Application.config.secret_token = 'f5b16f0fe6e1137f5b8ae3c27d1b2a0637f2d0b9f8aec3856ab3b14407b4881ec8bdf355949c58f3c75dfdc8761dd8cc1e8e3b63f21b2f737b6fff1d9acfd0a1'
