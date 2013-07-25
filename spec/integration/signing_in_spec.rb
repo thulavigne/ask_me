c1dbe5f15b3d0e703d8fba65a653c35993547667
Thu Quach <thuquach90@gmail.com>
2013-07-24 22:46:28 -0700

    Add feature for signing in via the Devise-provided form.

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 feature 'Signing in' do
   before do
     Factory(:user, :email => "ticketee@example.com")
@@ -6,8 +7,20 @@ feature 'Signing in' do
 
   scenario 'Signing in via confirmation' do
     open_email "ticketee@example.com", :with_subject => /Confirmation/
+    p User.first.confirmation_token
+    p current_email.body
     click_first_link_in_email
     page.should have_content("Your account was successfully confirmed")
     page.should have_content("Signed in as ticketee@example.com")
+  end
+
+  scenario 'Signing in via form' do
+    User.find_by_email('ticketee@example.com').confirm!
+    visit '/'
+    click_link 'Sign In'
+    fill_in 'Email', :with => "ticketee@example.com"
+    fill_in 'Password', :with => "password"
+    click_button "Sign in"
+    page.should have_content("Signed in successfully.")
   end
 end
