ca01a837a28d9fefa11f9fbcc40c40e6ea94e0ef
Thu Quach <thuquach90@gmail.com>
2013-07-24 22:38:40 -0700

    Add feature for ensuring that a user is signed in via a confirmation email by using email spec.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130725052411) do
+ActiveRecord::Schema.define(:version => 20130725053346) do
 
   create_table "discussions", :force => true do |t|
     t.text     "question"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(:version => 20130725052411) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
